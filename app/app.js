20e7d49e7ebacd675b2a204d9c99439b0ba56525
kvin1087 <kaustubh87@gmail.com>
2017-08-22 22:31:20 -0500

    Added bower_components

--- app/app.js
+++ app/app.js
@@ -1,14 +1,8 @@
-'use strict';
+(function() {
 
-// Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
+    'use strict';
 
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+    // Declare app level module which depends on views, and components
+    angular.module('readingList', []);
+
+})();
